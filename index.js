75601d1b5fafbc7012a4263dc862db829f5646f3
Renaldo112 <79891266+Renaldo112@users.noreply.github.com>
2021-05-07 08:29:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,89 @@
 const express = require('express');
 const app = express();
-const posts = require('./posts.json')
+const posts = require('./posts.json');
 const fs = require('fs');
-const { rawListeners } = require('process');
+
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-    res.send('hello world');
+app.get('/', function (req,res){
+    res.send('Hello world!');
 })
 
-app.get('/books', function (req, res) {
-    res.send('there are 4 books in store');
-});
+app.post('/', function(req,res){
+    res.send('This is a post request')
+})
 
-app.post('/', function (req, res) {
-    res.send('this is a post request')
-});
-
-app.get('/posts', (req, res) => {
+app.get('/posts', (req,res)=>{
     return res.json({ posts })
-
 })
 
-app.post('/posts', (req, res) => {
-   console.log(req.body.newPosts)
-    posts.push(req.body.newPosts);
+app.post('/posts',(req,res)=>{
+
+     posts.push(req.body.newPost);
+     console.log({ posts })
+
     let stringedData = JSON.stringify(posts, null, 2);
-    fs.writeFile('posts.json', stringedData, function (err) {
+    fs.writeFile('posts.json', stringedData, (err) => {
         if (err) {
-          return res.status(500).json({ message: err })
-      }
+            return res.status(500).json({message: err});
+        }        
+    });
+
+ return res.status(200).json({message: 'new post created'})
+});
+
+// fetch single post
+app.get('/posts/:id',(req, res)=>{
+
+    console.log(req.params.id);
+
+    let id = req.params.id;
+
+   let foundPost = posts.find(post =>{
+       return String(post.id) === id;
     })
-    
 
-    return res.status(200).json({ message:"new user created" })
+    if(foundPost){
+        return res.status(200).json({post:foundPost})   
+    }else{
+        return res.status(404).json({ message: "post not found"})
+    }
 })
 
-app.get('/posts/:id', (req, res) => {
-    
+app.put('/posts/:id',(req, res)=>{
+    //fetch req.params.id
+    console.log(req.params.id);
+
     let id = req.params.id;
-   let foundUser = posts.find(posts => {
-      return String(posts.id) === id
-   })
-    
-    
-    
-    if (foundUser) {
-       return res.status(200).json({posts: foundUser})
-    } else {
-        return res.status(404).json({message:"user not found"})
-   }
-    
-})
+    let updatedPost = req.body.updatedPost;
 
+    let updatePost = posts.find(post => String(post.id) === id)
+    if (updatePost){
+        updatePost.title = updatedPost.title;
+        updatePost.body = updatedPost.body;
 
-app.put('/posts/:id', (req, res) => {
+        let indexId = posts.indexOf(updatePost);
+        posts[indexId] = updatePost;
     
-    let id = req.params.id;
-   let foundUser = posts.find(posts => {
-      return String(posts.id) === id
-   })
-    
-    
-    
-    if (foundUser) {
-       return res.status(200).json({posts: foundUser})
-    } else {
-        return res.status(404).json({message:"user not found"})
-   }
-    
+       let stringedData = JSON.stringify(posts, null, 2);
+        fs.writeFile('posts.json', stringedData, (err) => {
+            if (err) {
+                return res.status(500).json({message: err});
+            }        
+        });
+
+        return res.status(200).json({post:updatePost});
+    }else{
+        return res.status(404).json({ message: "post could not be updated"})
+    }
 })
 
 
 
+let port = 5000;
 
-
-app.listen(3000, function () {
-    console.log('server is up and running')
+app.listen(port, function(){
+    console.log('server is up and running on port: ', port);
 })
